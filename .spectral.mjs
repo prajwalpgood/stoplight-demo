a1bde11d7dc55505125685d64db71d429f5d3c7a
prajwalp6991@gmail.com <prajwalp6991@gmail.com>
2024-03-18 12:22:46 +0530

    Modified 6 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0MDI0MA/spectral.js?branch_slug=main&token=59b38286-7335-4593-9ebc-e748bb22dabf";
+export default { extends: ruleset };
